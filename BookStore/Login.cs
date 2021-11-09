e6e1171ed4e393685c8069ae4179b0c8e620ce51
tuanntai <taifbeso@gmail.com>
2021-11-09 17:01:08 +0700

    Add Login Form

--- BookStore/Login.cs
+++ BookStore/Login.cs
@@ -10,17 +10,11 @@ using System.Windows.Forms;
 
 namespace BookStore
 {
-    public partial class Form1 : Form
+    public partial class Login : Form
     {
-        public Form1()
+        public Login()
         {
             InitializeComponent();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            Login login = new Login();
-            login.Show();
         }
     }
 }
