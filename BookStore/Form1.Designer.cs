945e59d608d38518528b3aa5503e64293fe67174
quocvinh <93987583+quocqinh@users.noreply.github.com>
2021-11-30 11:22:17 +0700

    alo

--- BookStore/Form1.Designer.cs
+++ BookStore/Form1.Designer.cs
@@ -39,6 +39,15 @@ namespace BookStore
             this.UserButton = new System.Windows.Forms.Button();
             this.HomeBtn = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.BookID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.AuthorName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.BookTitle = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Publisher = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Language = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Pic = new System.Windows.Forms.DataGridViewImageColumn();
+            this.Image = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Amount = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DataAdded = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel3.SuspendLayout();
@@ -55,8 +64,8 @@ namespace BookStore
             this.LoginBtn.FlatAppearance.BorderSize = 0;
             this.LoginBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.LoginBtn.ForeColor = System.Drawing.Color.White;
-            this.LoginBtn.Location = new System.Drawing.Point(1440, 26);
-            this.LoginBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.LoginBtn.Location = new System.Drawing.Point(1308, 14);
+            this.LoginBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.LoginBtn.Name = "LoginBtn";
             this.LoginBtn.Size = new System.Drawing.Size(221, 49);
             this.LoginBtn.TabIndex = 0;
@@ -74,7 +83,7 @@ namespace BookStore
             this.panel1.Controls.Add(this.LoginBtn);
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Location = new System.Drawing.Point(-7, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.MinimumSize = new System.Drawing.Size(2, 98);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1693, 98);
@@ -83,7 +92,7 @@ namespace BookStore
             // panel2
             // 
             this.panel2.Location = new System.Drawing.Point(4, 98);
-            this.panel2.Margin = new System.Windows.Forms.Padding(4);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(267, 123);
             this.panel2.TabIndex = 3;
@@ -94,7 +103,7 @@ namespace BookStore
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pictureBox1.Location = new System.Drawing.Point(4, -1);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(373, 98);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -112,7 +121,7 @@ namespace BookStore
             this.panel3.Controls.Add(this.UserButton);
             this.panel3.Controls.Add(this.HomeBtn);
             this.panel3.Location = new System.Drawing.Point(-1, 97);
-            this.panel3.Margin = new System.Windows.Forms.Padding(4);
+            this.panel3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(373, 840);
             this.panel3.TabIndex = 3;
@@ -123,7 +132,7 @@ namespace BookStore
             this.ExitButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.ExitButton.Location = new System.Drawing.Point(1, 788);
-            this.ExitButton.Margin = new System.Windows.Forms.Padding(4);
+            this.ExitButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.ExitButton.Name = "ExitButton";
             this.ExitButton.Size = new System.Drawing.Size(368, 49);
             this.ExitButton.TabIndex = 10;
@@ -134,7 +143,7 @@ namespace BookStore
             // BookBtn
             // 
             this.BookBtn.Location = new System.Drawing.Point(3, 106);
-            this.BookBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.BookBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.BookBtn.Name = "BookBtn";
             this.BookBtn.Size = new System.Drawing.Size(368, 49);
             this.BookBtn.TabIndex = 8;
@@ -146,7 +155,7 @@ namespace BookStore
             // 
             this.UserButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(254)))), ((int)(((byte)(254)))));
             this.UserButton.Location = new System.Drawing.Point(1, 53);
-            this.UserButton.Margin = new System.Windows.Forms.Padding(4);
+            this.UserButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.UserButton.Name = "UserButton";
             this.UserButton.Size = new System.Drawing.Size(368, 49);
             this.UserButton.TabIndex = 7;
@@ -158,7 +167,7 @@ namespace BookStore
             // 
             this.HomeBtn.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(234)))), ((int)(((byte)(236)))));
             this.HomeBtn.Location = new System.Drawing.Point(1, 1);
-            this.HomeBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.HomeBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.HomeBtn.Name = "HomeBtn";
             this.HomeBtn.Size = new System.Drawing.Size(368, 49);
             this.HomeBtn.TabIndex = 6;
@@ -173,24 +182,109 @@ namespace BookStore
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(234)))), ((int)(((byte)(236)))));
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.BookID,
+            this.AuthorName,
+            this.BookTitle,
+            this.Publisher,
+            this.Language,
+            this.Pic,
+            this.Image,
+            this.Amount,
+            this.DataAdded});
             this.dataGridView1.Location = new System.Drawing.Point(373, 97);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.Size = new System.Drawing.Size(1313, 841);
             this.dataGridView1.TabIndex = 4;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
+            // BookID
+            // 
+            this.BookID.DataPropertyName = "BookID";
+            this.BookID.HeaderText = "BookID";
+            this.BookID.MinimumWidth = 6;
+            this.BookID.Name = "BookID";
+            this.BookID.Width = 125;
+            // 
+            // AuthorName
+            // 
+            this.AuthorName.DataPropertyName = "AuthorName";
+            this.AuthorName.HeaderText = "AuthorName";
+            this.AuthorName.MinimumWidth = 6;
+            this.AuthorName.Name = "AuthorName";
+            this.AuthorName.Width = 125;
+            // 
+            // BookTitle
+            // 
+            this.BookTitle.DataPropertyName = "BookTitle";
+            this.BookTitle.HeaderText = "BookTitle";
+            this.BookTitle.MinimumWidth = 6;
+            this.BookTitle.Name = "BookTitle";
+            this.BookTitle.Width = 125;
+            // 
+            // Publisher
+            // 
+            this.Publisher.DataPropertyName = "Publisher";
+            this.Publisher.HeaderText = "Publisher";
+            this.Publisher.MinimumWidth = 6;
+            this.Publisher.Name = "Publisher";
+            this.Publisher.Width = 125;
+            // 
+            // Language
+            // 
+            this.Language.DataPropertyName = "Language";
+            this.Language.HeaderText = "Language";
+            this.Language.MinimumWidth = 6;
+            this.Language.Name = "Language";
+            this.Language.Width = 125;
+            // 
+            // Pic
+            // 
+            this.Pic.DataPropertyName = "Pic";
+            this.Pic.HeaderText = "Pic";
+            this.Pic.MinimumWidth = 6;
+            this.Pic.Name = "Pic";
+            this.Pic.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Pic.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.Pic.Width = 125;
+            // 
+            // Image
+            // 
+            this.Image.DataPropertyName = "Image";
+            this.Image.HeaderText = "Image";
+            this.Image.MinimumWidth = 6;
+            this.Image.Name = "Image";
+            this.Image.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Image.Width = 125;
+            // 
+            // Amount
+            // 
+            this.Amount.DataPropertyName = "Amount";
+            this.Amount.HeaderText = "Amount";
+            this.Amount.MinimumWidth = 6;
+            this.Amount.Name = "Amount";
+            this.Amount.Width = 125;
+            // 
+            // DataAdded
+            // 
+            this.DataAdded.DataPropertyName = "DataAdded";
+            this.DataAdded.HeaderText = "DataAdded";
+            this.DataAdded.MinimumWidth = 6;
+            this.DataAdded.Name = "DataAdded";
+            this.DataAdded.Width = 125;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1685, 937);
+            this.ClientSize = new System.Drawing.Size(1540, 846);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -214,6 +308,15 @@ namespace BookStore
         private System.Windows.Forms.Button BookBtn;
         private System.Windows.Forms.Button UserButton;
         private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.DataGridViewTextBoxColumn BookID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn AuthorName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn BookTitle;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Publisher;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Language;
+        private System.Windows.Forms.DataGridViewImageColumn Pic;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Image;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Amount;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DataAdded;
     }
 }
 
